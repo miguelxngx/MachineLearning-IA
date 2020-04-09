4d2f71d54a0480a25598c1238f13c9edfbac1fa8
miguelxngx <A01350836@itesm.mx>
2020-04-08 20:07:39 -0500

    logistic regression with multiproccessing

--- LogisticRegressionProject/Program.py
+++ LogisticRegressionProject/Program.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import multiprocessing
+import multiprocessing as mp
+import random
 
 from absl import app
 from random import seed
@@ -9,7 +10,10 @@ from csv import reader
 from math import exp
 from keras.utils import to_categorical
 from joblib import Parallel, delayed
+
 __errors__ = []
+__temp_params__ = []
+__h__ = []
 
 #Load CSV file
 def load_csv(filename):
@@ -63,8 +67,9 @@ def get_error(samples, params,y):
     global __errors__
     error_acum =0
     error = 0
+    global __h__
     for i in range(len(samples)):
-        hyp = h(params,samples[i])
+        hyp = __h__[i]
         
         if(y[i] == 1): # avoid the log(0) error
             if(hyp == 0):
@@ -79,102 +84,125 @@ def get_error(samples, params,y):
     mean_error_param = error_acum / len(samples)
     __errors__.append(mean_error_param)
     return mean_error_param
-    
-def h(samples, params):
+
+def calculate_h(instance, params):
     acum = 0
     for i in range(len(params)):
-        acum = acum + params[i]*samples[i]
+        acum = acum + params[i]*instance[i]
     acum = acum*(-1)
     acum = 1/(1+ exp(acum))
     return acum
 
-def GD(samples, params, y, alpha):
-    temp_params=list(params)
-    for i in range(len(params)):
-        acum=0
-        for j in range(len(samples)):
-            error = h(params,samples[j]) - y[j]
-            acum = acum + error*samples[j][i]
-        temp_params[i] = params[i] - alpha*(1/len(samples))*acum
-    return temp_params
+def GD(samples, params, y, alpha,index):
+    temp_param = params[index]
+    global __h__
+    acum=0
+    for j in range(len(samples)):
+        error = __h__[j] - y[j]
+        acum = acum + error*samples[j][index]
+    temp_param = params[index] - alpha*(1/len(samples))*acum
 
-def main(useless_argv):
+    return temp_param   
+        
+
+if __name__ == "__main__":
+    mp.freeze_support()
     filename = "agaricus-lepiota.data"
     dataset = load_csv(filename)
 
     y = encodeYs(dataset[:,0])
     samples = np.ones(len(dataset))
 
-    #One hot encode first column
-    column = dataset[:,1]
-    column = encode_label(column)
-    encoded_column = to_categorical(column)
-    samples = np.c_[samples, encoded_column]
-
-    #One hot encode third column
-    column = dataset[:,3]
-    column = encode_label(column)
-    encoded_column = to_categorical(column)
-    samples = np.c_[samples, encoded_column]
-
-    #One hot encode fifth column
+    #odor
     column = dataset[:,5]
     column = encode_label(column)
     encoded_column = to_categorical(column)
     samples = np.c_[samples, encoded_column]
 
-    #One hot encode fifth column
-    column = dataset[:,13]
-    column = encode_label(column)
-    encoded_column = to_categorical(column)
-    samples = np.c_[samples, encoded_column]
-
-    #One hot encode fifth column
-    column = dataset[:,14]
-    column = encode_label(column)
-    encoded_column = to_categorical(column)
-    samples = np.c_[samples, encoded_column]
-
-    #One hot encode twentieth column
+    #spore print
     column = dataset[:,20]
     column = encode_label(column)
     encoded_column = to_categorical(column)
     samples = np.c_[samples, encoded_column]
 
-    #One hot encode fifth column
-    column = dataset[:,21]
+    #stalk surface bellow ring
+    column = dataset[:,13]
     column = encode_label(column)
     encoded_column = to_categorical(column)
     samples = np.c_[samples, encoded_column]
 
-    #One hot encode wenty-seconth column
+    #stalk color above ring
+    column = dataset[:,14]
+    column = encode_label(column)
+    encoded_column = to_categorical(column)
+    samples = np.c_[samples, encoded_column]
+
+    #habitat
     column = dataset[:,22]
     column = encode_label(column)
     encoded_column = to_categorical(column)
     samples = np.c_[samples, encoded_column]
 
+    #cap color
+    column = dataset[:,3]
+    column = encode_label(column)
+    encoded_column = to_categorical(column)
+    samples = np.c_[samples, encoded_column]
+
+    #population
+    column = dataset[:,21]
+    column = encode_label(column)
+    encoded_column = to_categorical(column)
+    samples = np.c_[samples, encoded_column]
+
+    #gill color
+    column = dataset[:,9]
+    column = encode_label(column)
+    encoded_column = to_categorical(column)
+    samples = np.c_[samples, encoded_column]
+
+    #ring number
+    column = dataset[:,18]
+    column = encode_label(column)
+    encoded_column = to_categorical(column)
+    samples = np.c_[samples, encoded_column]
+
+    #ring type
+    column = dataset[:,19]
+    column = encode_label(column)
+    encoded_column = to_categorical(column)
+    samples = np.c_[samples, encoded_column]
+
+    printToFile(samples, "test")
+
     params = [0]*len(samples[0])
     print(len(params))
 
-    alpha = 0.03
-    samples = samples.tolist()
+    alpha = 0.8
+    random.shuffle(samples)
+    train_samples = samples[0:, :]
+    train_samples = train_samples.tolist()
+    test_samples = samples[4000:,:]
     epoch = 1
+    __h__ = [0]*len(train_samples)
+    num_cores = mp.cpu_count()
     while True:
         old_params = list(params)
         #print(params)
-        params = GD(samples,params,y,alpha)
-        error = get_error(samples,params,y)
+        #print("calculating h...")
+        __h__ = Parallel(n_jobs=num_cores, require='sharedmem')(delayed(calculate_h)(instance, params) for instance in train_samples)
+        #print("calculating parameters...")
+        params = Parallel(n_jobs=num_cores, require='sharedmem')(delayed(GD)(train_samples, params, y, alpha, i) for i in range(len(params)))
+        #print("calculating error...")
+        print(params)
+        error = get_error(train_samples,params,y)
         print("Epoch: ", epoch, " error: ", error)
         epoch = epoch + 1
         
         if(old_params == params or error < 0.0001):
-            printToFile(samples, "Samples")
+            printToFile(train_samples, "Samples")
             print ("final params:")
             print (params)
             break
     plt.plot(__errors__)
     plt.show()
-        
-
-if __name__ == "__main__":
-    app.run(main)
